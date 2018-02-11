c650a6650a28bd8cb9e2b93106a29dc402d6bc8d
Trevor Bennett <garbage@trevorbennett.us>
2018-02-11 13:55:26 -0500

    move/remove unused files

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ var http = require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World!');
-}).listen(8080); 
+    res.end('Original String do not steal!');
+}).listen(8080);

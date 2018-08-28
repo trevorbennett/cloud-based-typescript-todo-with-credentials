5fbbc710a97ab0b0bce07ba6eba0352179f83684
Trevor Bennett <github@trevorbennett.us>
2018-08-28 19:18:22 -0400

    Grid model updated to match expected MVP

--- grid.js
+++ grid.js
@@ -7,36 +7,36 @@ $(function() {
     editing: true,
     sorting: true,
     paging: true,
-    data: friends,
+    data: gridElements,
     fields: [
       { name: "Done", type: "checkbox", width: 20, title: "Done" },
-      { name: "Name", type: "text", width: 150 },
-      { name: "CompleteBy", type: "date", width: 50 },
-      countries,
+      { name: "Task", type: "text", width: 150 },
+      { name: "CompleteBy", type: "date", width: 50, title: "Complete By" },
+      project,
       { type: "control" }
     ]
   })  
 })
 
-var friends = [
+var gridElements = [
   {
-    Name: "John Adams",
+    Task: "Make a Todo list",
     CompleteBy: "3-6-1993",
-    Country: 1,
-    Cool: false,
+    Project: 1,
+    Done: false,
   }
 ];
 
-var countries = {
-  name: "Country",
+var project = {
+  name: "Project",
   type: "select",
   items: [
     { Name: "", Id: 0 },
-    { Name: "United States", Id: 1 },
-    { Name: "France", Id: 2 },
-    { Name: "United Kingdom", Id: 3 },
-    { Name: "Spain", Id: 4 },
-    { Name: "Mexico", Id: 5 }
+    { Name: "Work", Id: 1 },
+    { Name: "Home", Id: 2 },
+    { Name: "Volunteering", Id: 3 },
+    { Name: "Self Improvement", Id: 4 },
+    { Name: "Community", Id: 5 }
   ],
   valueField: "Id",
   textField: "Name"
